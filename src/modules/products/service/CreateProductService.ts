b191c17c0308dd855a9c05e7d03763a4c98210b7
Italo Vieira <ItaloV@pma.local>
2022-12-06 13:34:16 -0300

    [Feature: Italo, Task: criacao do projeto api-vendas e todo o crud de products]

---
+++ src/modules/products/service/CreateProductService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const ProductExists = await productsRepository.findByName(name);
+
+    if (ProductExists) {
+      throw new AppError('there id alredy one product with thi name');
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,9 @@
+import productsRouter from "@modules/products/routes/products.routes";
+import { response, Router } from "express";
+import { request } from "http";
+
+const routes = Router();
+
+routes.use('/products', productsRouter);
+
+export default routes;
