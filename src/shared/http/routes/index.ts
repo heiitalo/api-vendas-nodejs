d30511d384028f39f0cb0a0f433b35c6de337fe0
Italo Vieira <ItaloV@pma.local>
2022-12-12 09:16:53 -0300

    Feature Italo Task: criacao das rotas de token e redefinicao de senha

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,12 +1,14 @@
 import productsRouter from "@modules/products/routes/products.routes";
+import passwordRouter from "@modules/users/routes/password.routes";
 import sessionsRouter from "@modules/users/routes/sessions.routes";
 import userRouter from "@modules/users/routes/users.routes";
 import { response, Router } from "express";
-import { request } from "http";
+
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions',sessionsRouter);
+routes.use('/password', passwordRouter)
 export default routes;
